5f560ab83b4e0324576c2cccb8baa2b67e2c6a1c
Junior Carvalho <antoniomarcosdecj18@gmail.com>
2022-08-31 16:01:27 -0300

    first implementation of password-generator done

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+// Seleção de elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+// Funções
+const getLetterLowerCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+const getLetterUpperCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+const getNumber = () => {
+  return Math.floor(Math.random() * 10).toString();
+}
+
+const getSymbol = () => {
+  const symbols = "(){}[]=<>/,.!@#$%&*+-";
+  return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
+  let password = '';
+
+  const passwordLength = 10;
+
+  const generators = [
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol,
+    getLetterLowerCase
+  ]
+
+  for(i = 0; i < passwordLength; i += generators.length) {
+    generators.forEach(() => {
+      const randomValue = generators[Math.floor(Math.random() * generators.length)]();
+
+      password += randomValue;
+    })
+  }
+  generatedPasswordElement.style.display = "block";
+  generatedPasswordElement.querySelector("h4").innerText = password;
+}
+
+// Eventos
+generatePasswordButton.addEventListener("click", () => {
+  generatePassword(
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol);
+});
